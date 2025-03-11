59e4fd6ab13e5cb81304f288ff57ac7bb1101756
Nicholas Pimenta <nicholas2ilva@gmail.com>
2025-03-10 21:51:46 -0300

    Added Hover and color to Link Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-dark text-white text-center py-3 mt-5">
       <p>&copy; 2025 InfoPC. Todos os direitos reservados.</p>
       <div>
-        <a href="https://www.instagram.com/loja_informatica" className="text-white me-3" target="_blank" rel="noopener noreferrer">
+        <a
+          href="#"
+          className="text-white link-primary me-3 text-decoration-none"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Instagram
         </a>
-        <a href="https://www.facebook.com/loja_informatica" className="text-white me-3" target="_blank" rel="noopener noreferrer">
+        <a
+          href="#"
+          className="text-white link-primary me-3 text-decoration-none"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Facebook
         </a>
-        <a href="mailto:contato@loja-informatica.com.br" className="text-white">
+        <a
+          href="mailto:contato@infoPC.com.br"
+          className="text-white link-primary text-decoration-none"
+        >
           E-mail
         </a>
       </div>
